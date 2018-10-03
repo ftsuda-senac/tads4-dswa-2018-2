4bc64f1070302f41a89802824a103d1e8f4a5b6b
Fernando Tsuda <ftsuda.senac@gmail.com>
2018-10-02 22:41:42 -0300

    Exemplo upload de arquivo

--- exemplos-spring/src/main/java/br/senac/tads4/dsw/exemplosspring/ExemplosSpringApplication.java
+++ exemplos-spring/src/main/java/br/senac/tads4/dsw/exemplosspring/ExemplosSpringApplication.java
@@ -2,11 +2,22 @@ package br.senac.tads4.dsw.exemplosspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+// Para gerar WAR a partir do projeto Spring Boot
+// https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#howto-create-a-deployable-war-file
+// https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#build-tool-plugins-maven-packaging
 
 @SpringBootApplication
-public class ExemplosSpringApplication {
+public class ExemplosSpringApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExemplosSpringApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ExemplosSpringApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExemplosSpringApplication.class, args);
+    }
 }
